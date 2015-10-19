69eda05287560855383b01911e6c186249dda203
HIgashikawaMasa <necromancer393@live.jp>
2015-10-20 01:34:26 +0900

    on_off

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//シェルスプリクト用
 var exec = require('child_process').exec;
 
 // wwwディレクトリを静的ファイルディレクトリとして登録
@@ -13,6 +14,7 @@ app.use(express.static('www'));
 
 // サーバを開始
 server.listen(process.env.PORT || 3000);
+
 
 app.get('/on', function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
@@ -30,7 +32,16 @@ app.get('/off', function(req, res) {
 
 io.on('connection', function (socket) {
 	console.log("新規接続がありました。");
+	socket.on("text", function (data) {
+		if(data==1){
+			exec('echo 1 > /sys/class/gpio/gpio18/value');
+		}
+		else{
+			exec('echo 0 > /sys/class/gpio/gpio18/value');
+		}
+		
+	});
 	
-	exec('echo 1 > /sys/class/gpio/gpio18/value');
+	
 	
 });
