56003e340be41d6bf0bf0823f2d0f2416cebc7a5
HIgashikawaMasa <necromancer393@live.jp>
2015-10-20 01:00:56 +0900

    on_off

--- server.js
+++ server.js
@@ -14,7 +14,19 @@ app.use(express.static('www'));
 // サーバを開始
 server.listen(process.env.PORT || 3000);
 
+app.get('/on', function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
+	res.write("ON");
+   	res.end();
+	exec('echo 1 > /sys/class/gpio/gpio18/value');
+});
 
+app.get('/off', function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
+	res.write("OFF");
+   	res.end();
+	exec('echo 0 > /sys/class/gpio/gpio18/value');
+});
 
 io.on('connection', function (socket) {
 	console.log("新規接続がありました。");
