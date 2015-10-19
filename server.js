24c1bec4bd401d4e81ddb25f193c8ef20fc4eec8
HIgashikawaMasa <necromancer393@live.jp>
2015-10-20 00:52:54 +0900

    fix

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var furagu;
+
 // wwwディレクトリを静的ファイルディレクトリとして登録
 app.use(express.static('www'));
 
 // サーバを開始
 server.listen(process.env.PORT || 3000);
 
+
+
 io.on('connection', function (socket) {
 	console.log("新規接続がありました。");
+	
+	exec('echo 1 > /sys/class/gpio/gpio18/value', function(err, stdout, stderr){});
+	
 });
